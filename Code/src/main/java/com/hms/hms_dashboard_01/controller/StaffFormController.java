607bff08eba86e414c994328e92d3819e0ccaeab
Muhammad Irfan <irfanum512@gmail.com>
2023-06-13 14:52:27 +0500

    Added Staff Controller

---
+++ Code/src/main/java/com/hms/hms_dashboard_01/controller/StaffFormController.java
@@ -0,0 +1,13 @@
+package com.hms.hms_dashboard_01.controller;
+
+import javafx.event.ActionEvent;
+
+public class StaffFormController {
+
+
+
+ public void addStaff (ActionEvent e){
+     System.out.println("Staff Added");
+ }
+
+}
