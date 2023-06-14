9a1b9b784952aafa7b60d21d245e471c87a49d0f
Hanzla Nouman <64569364+HanzlaNouman@users.noreply.github.com>
2023-06-14 06:33:26 +0500

    Added forms and Room Controller

---
+++ RoomFormController.java
@@ -0,0 +1,14 @@
+package com.hms.hms_dashboard_01.controller.forms;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class RoomFormController {
+
+
+
+    public void addRoom(ActionEvent e){
+        System.out.println("Add Room");
+
+    }
+}
