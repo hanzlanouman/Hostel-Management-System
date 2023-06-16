6eb0035f66c39bde3d26165af908f3fffec7295c
Hanzla Nouman <64569364+HanzlaNouman@users.noreply.github.com>
2023-06-16 10:26:59 +0500

    Implemented Validation Logic

--- Code/src/main/java/com/hms/hms_dashboard_01/Validators/RoomFormValidator.java
+++ Code/src/main/java/com/hms/hms_dashboard_01/Validators/RoomFormValidator.java
@@ -4,24 +4,8 @@ import com.hms.hms_dashboard_01.DTO.RoomDTO;
 
 public class RoomFormValidator {
 
-   public static void isValidInfo(RoomDTO room){
-         if(room.getRoomNo() == 0){
-              System.out.println("Room number cannot be empty");
-         }
-            if(room.getRoomType() == null){
-                System.out.println("Room type cannot be empty");
-            }
-            if(room.getRoomFloor() == null){
-                System.out.println("Room floor cannot be empty");
-            }
-            if(room.getRoomAvb() == null){
-                System.out.println("Room availability cannot be empty");
-            }
-         if(room.getRoomFee() == 0){
-              System.out.println("Room fee cannot be empty");
-         }
-         if(room.getRoomBuilding() == null){
-              System.out.println("Room building cannot be empty");
-         }
-   }
-}
+   public static boolean isValidInfo(RoomDTO room){
+        return room.getRoomNo() != 0 && room.getRoomFee() != 0 && room.getRoomType() != null && room.getRoomFloor() != null && room.getRoomAvb() != null && room.getRoomBuilding() != null;
+
+}}
+
