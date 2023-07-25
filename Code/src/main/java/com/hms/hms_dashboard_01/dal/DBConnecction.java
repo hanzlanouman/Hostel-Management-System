34aa8b740960d2e2f593001905609c54b944ddd0
Hanzla Nouman <64569364+HanzlaNouman@users.noreply.github.com>
2023-07-25 13:16:06 +0500

    Fixed more DB errors

--- Code/src/main/java/com/hms/hms_dashboard_01/dal/DBConnecction.java
+++
@@ -1,9 +0,0 @@
-package com.hms.hms_dashboard_01.dal;
-
-import java.sql.Connection;
-
-public interface DBConnecction {
-    public Connection getConnection();
-    public void closeConnection(Connection conn);
-
-}
